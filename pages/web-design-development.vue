47beb4c01858c67bbfef93b092b19d7c01401f87
AntixxTechHub <info@antixxtechhub.com>
2022-12-18 00:21:12 +0530

    Web design page filtered

--- pages/web-design-development.vue
+++ pages/web-design-development.vue
@@ -1,16 +1,15 @@
 <template>
     <div>
         <NavbarStyleTwo />
-        <PageTitle pageTitle="Web Design & Development" pageDesc="Lorem Ipsum" />
-        <About />
-        <MoreToDiscover />
-        <ServicesOne />
-        <AmazingFeatures />
-        <WhyChooseUs />
-        <TheBestInnovative />
-        <ConnectionsYourCustomers />
+        <PageTitle pageTitle="Web Design & Development" pageDesc="" />
+        <WebDesignAbout />
+        <FutureReadyWebsite />
+        <TopTechnology />
+        <ServicesByAntixxTechHub />
+        <WeOfferTrade />
+        <ApproachforWebsiteDesign />
+        <WhyChooseAntixxTechHub />
         <Faq />
-        <!-- <Pricing /> -->
         <BestSupport />
         <FreeTrial />
         <FooterStyleOne />
@@ -19,51 +18,31 @@
 
 <script>
 import NavbarStyleTwo from '../layouts/NavbarStyleTwo';
-import PageTitle from '../components/web-design-development/PageTitle';
-import AmazingFeatures from '../components/web-design-development/AmazingFeatures';
-import ServicesOne from '../components/web-design-development/ServicesOne';
-import MoreToDiscover from '../components/web-design-development/MoreToDiscover';
-import Services from '../components/web-design-development/Services';
-import YourApplication from '../components/web-design-development/YourApplication';
-import TheAbilityToElicit from '../components/web-design-development/TheAbilityToElicit';
-import TheBestInnovative from '../components/web-design-development/TheBestInnovative';
-import WhyChooseUs from '../components/web-design-development/WhyChooseUs';
-import Features from '../components/web-design-development/Features';
-import ConnectionsYourCustomers from '../components/web-design-development/ConnectionsYourCustomers';
-import About from '../components/web-design-development/About';
-import PageAbout from '../components/web-design-development/PageAbout';
-import CaseStudies from '../components/web-design-development/CaseStudies';
-import Featured from '../components/web-design-development/Featured';
-import PageFeature from '../components/web-design-development/PageFeature';
-import Expertise from '../components/web-design-development/Expertise';
+import PageTitle from '../components/Common/PageTitle';
+import WebDesignAbout from '../components/web-design-development/WebDesignAbout';
+import FutureReadyWebsite from '../components/web-design-development/FutureReadyWebsite';
+import TopTechnology from '../components/web-design-development/TopTechnology';
+import ServicesByAntixxTechHub from '../components/web-design-development/ServicesByAntixxTechHub';
+import WeOfferTrade from '../components/web-design-development/WeOfferTrade';
+import ApproachforWebsiteDesign from '../components/web-design-development/ApproachforWebsiteDesign';
+import WhyChooseAntixxTechHub from '../components/web-design-development/WhyChooseAntixxTechHub';
 import Faq from '../components/web-design-development/Faq';
-import Pricing from '../components/web-design-development/Pricing';
-import BestSupport from '../components/web-design-development/BestSupport';
-import FreeTrial from '../components/web-design-development/FreeTrial';
+import BestSupport from '../components/Common/BestSupport';
+import FreeTrial from '../components/Common/FreeTrial';
 import FooterStyleOne from '../layouts/FooterStyleOne';
 
 export default {
     components: {
         NavbarStyleTwo,
         PageTitle,
-        AmazingFeatures,
-        ServicesOne,
-        Services,
-        MoreToDiscover,
-        YourApplication,
-        TheAbilityToElicit,
-        WhyChooseUs,
-        Features,
-        ConnectionsYourCustomers,
-        TheBestInnovative,
-        About,
-        PageAbout,
-        PageFeature,
-        CaseStudies,
-        Featured,
-        Expertise,
+        WebDesignAbout,
+        FutureReadyWebsite,
+        TopTechnology,
+        ServicesByAntixxTechHub,
+        WeOfferTrade,
+        ApproachforWebsiteDesign,
+        WhyChooseAntixxTechHub,
         Faq,
-        Pricing,
         BestSupport,
         FreeTrial,
         FooterStyleOne,
