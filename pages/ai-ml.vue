afda2ac9eb7999ced6593cc84490f2c20d2e912f
AntixxTechHub <info@antixxtechhub.com>
2022-12-18 15:48:08 +0530

    AI ML page filtered 18.12.22

--- pages/ai-ml.vue
+++ pages/ai-ml.vue
@@ -1,13 +1,12 @@
 <template>
     <div>
         <NavbarStyleTwo />
-        <PageTitle pageTitle="AI & ML" pageDesc="Lorem Ipsum" />
-        <About />
-        <MoreToDiscover />
-        <Services />
-        <PageFeature />
-        <WhyChooseUs />
-        <!-- <Faq /> -->
+        <PageTitle pageTitle="AI & ML" pageDesc="" />
+        <AiMlAbout />
+        <ImportanceofArtificial />
+        <TechnologiesTools />
+        <WhyHireAntixxTechhub />
+        <WeOfferAIML />
         <BestSupport />
         <FreeTrial />
         <FooterStyleOne />
@@ -16,51 +15,25 @@
 
 <script>
 import NavbarStyleTwo from '../layouts/NavbarStyleTwo';
-import PageTitle from '../components/ai-ml/PageTitle';
-import AmazingFeatures from '../components/ai-ml/AmazingFeatures';
-import ServicesOne from '../components/ai-ml/ServicesOne';
-import MoreToDiscover from '../components/ai-ml/MoreToDiscover';
-import Services from '../components/ai-ml/Services';
-import YourApplication from '../components/ai-ml/YourApplication';
-import TheAbilityToElicit from '../components/ai-ml/TheAbilityToElicit';
-import TheBestInnovative from '../components/ai-ml/TheBestInnovative';
-import WhyChooseUs from '../components/ai-ml/WhyChooseUs';
-import Features from '../components/ai-ml/Features';
-import ConnectionsYourCustomers from '../components/ai-ml/ConnectionsYourCustomers';
-import About from '../components/ai-ml/About';
-import PageAbout from '../components/ai-ml/PageAbout';
-import CaseStudies from '../components/ai-ml/CaseStudies';
-import Featured from '../components/ai-ml/Featured';
-import PageFeature from '../components/ai-ml/PageFeature';
-import Expertise from '../components/ai-ml/Expertise';
-import Faq from '../components/ai-ml/Faq';
-import Pricing from '../components/ai-ml/Pricing';
-import BestSupport from '../components/ai-ml/BestSupport';
-import FreeTrial from '../components/ai-ml/FreeTrial';
+import PageTitle from '../components/Common/PageTitle';
+import AiMlAbout from '../components/ai-ml/AiMlAbout';
+import ImportanceofArtificial from '../components/ai-ml/ImportanceofArtificial';
+import TechnologiesTools from '../components/ai-ml/TechnologiesTools';
+import WhyHireAntixxTechhub from '../components/ai-ml/WhyHireAntixxTechhub';
+import WeOfferAIML from '../components/ai-ml/WeOfferAIML';
+import BestSupport from '../components/Common/BestSupport';
+import FreeTrial from '../components/Common/FreeTrial';
 import FooterStyleOne from '../layouts/FooterStyleOne';
 
 export default {
     components: {
         NavbarStyleTwo,
         PageTitle,
-        AmazingFeatures,
-        ServicesOne,
-        Services,
-        MoreToDiscover,
-        YourApplication,
-        TheAbilityToElicit,
-        WhyChooseUs,
-        Features,
-        ConnectionsYourCustomers,
-        TheBestInnovative,
-        About,
-        PageAbout,
-        PageFeature,
-        CaseStudies,
-        Featured,
-        Expertise,
-        Faq,
-        Pricing,
+        AiMlAbout,
+        ImportanceofArtificial,
+        TechnologiesTools,
+        WhyHireAntixxTechhub,
+        WeOfferAIML,
         BestSupport,
         FreeTrial,
         FooterStyleOne,
