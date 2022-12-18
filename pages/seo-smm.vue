c93ecd6bea3ac0cf866bba4b33078ed3970f269f
AntixxTechHub <info@antixxtechhub.com>
2022-12-18 13:00:39 +0530

    SEO SMO page filtered 18.22.22

--- pages/seo-smm.vue
+++ pages/seo-smm.vue
@@ -1,16 +1,15 @@
 <template>
     <div>
         <NavbarStyleTwo />
-        <PageTitle pageTitle="SEO SMM" pageDesc="Lorem Ipsum" />
-        <About />
-        <MoreToDiscover />
-        <PageFeature />
-        <WhyChooseUs />
-        <TheBestInnovative />
-        <ConnectionsYourCustomers />
-        <PageAbout />
+        <PageTitle pageTitle="SEO SMM" pageDesc="" />
+        <SeoSmoAbout />
+        <WhyYouNeed />
+        <ScopeofTechnical />
+        <ScopeofWebsite />
+        <QualitySEO />
+        <OurApproach />
+        <WhyHireAntixxTechHub />
         <Faq />
-        <!-- <Pricing /> -->
         <BestSupport />
         <FreeTrial />
         <FooterStyleOne />
@@ -19,51 +18,31 @@
 
 <script>
 import NavbarStyleTwo from '../layouts/NavbarStyleTwo';
-import PageTitle from '../components/seo-smm/PageTitle';
-import AmazingFeatures from '../components/seo-smm/AmazingFeatures';
-import ServicesOne from '../components/seo-smm/ServicesOne';
-import MoreToDiscover from '../components/seo-smm/MoreToDiscover';
-import Services from '../components/seo-smm/Services';
-import YourApplication from '../components/seo-smm/YourApplication';
-import TheAbilityToElicit from '../components/seo-smm/TheAbilityToElicit';
-import TheBestInnovative from '../components/seo-smm/TheBestInnovative';
-import WhyChooseUs from '../components/seo-smm/WhyChooseUs';
-import Features from '../components/seo-smm/Features';
-import ConnectionsYourCustomers from '../components/seo-smm/ConnectionsYourCustomers';
-import About from '../components/seo-smm/About';
-import PageAbout from '../components/seo-smm/PageAbout';
-import CaseStudies from '../components/seo-smm/CaseStudies';
-import Featured from '../components/seo-smm/Featured';
-import PageFeature from '../components/seo-smm/PageFeature';
-import Expertise from '../components/seo-smm/Expertise';
+import PageTitle from '../components/Common/PageTitle';
+import SeoSmoAbout from '../components/seo-smm/SeoSmoAbout';
+import WhyYouNeed from '../components/seo-smm/WhyYouNeed';
+import ScopeofTechnical from '../components/seo-smm/ScopeofTechnical';
+import ScopeofWebsite from '../components/seo-smm/ScopeofWebsite';
+import QualitySEO from '../components/seo-smm/QualitySEO';
+import OurApproach from '../components/seo-smm/OurApproach';
+import WhyHireAntixxTechHub from '../components/seo-smm/WhyHireAntixxTechHub';
 import Faq from '../components/seo-smm/Faq';
-import Pricing from '../components/seo-smm/Pricing';
-import BestSupport from '../components/seo-smm/BestSupport';
-import FreeTrial from '../components/seo-smm/FreeTrial';
+import BestSupport from '../components/Common/BestSupport';
+import FreeTrial from '../components/Common/FreeTrial';
 import FooterStyleOne from '../layouts/FooterStyleOne';
 
 export default {
     components: {
         NavbarStyleTwo,
         PageTitle,
-        AmazingFeatures,
-        ServicesOne,
-        Services,
-        MoreToDiscover,
-        YourApplication,
-        TheAbilityToElicit,
-        WhyChooseUs,
-        Features,
-        ConnectionsYourCustomers,
-        TheBestInnovative,
-        About,
-        PageAbout,
-        PageFeature,
-        CaseStudies,
-        Featured,
-        Expertise,
+        SeoSmoAbout,
+        WhyYouNeed,
+        ScopeofTechnical,
+        ScopeofWebsite,
+        QualitySEO,
+        OurApproach,
+        WhyHireAntixxTechHub,
         Faq,
-        Pricing,
         BestSupport,
         FreeTrial,
         FooterStyleOne,
