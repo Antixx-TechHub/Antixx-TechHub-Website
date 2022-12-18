e4bd6ba38238d0e50439d48ae8af49ff17f0ea84
AntixxTechHub <info@antixxtechhub.com>
2022-12-18 16:47:33 +0530

    Staff Augmentation page filtered 18.12.22

--- pages/staff-augmentation.vue
+++ pages/staff-augmentation.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <NavbarStyleTwo />
-        <PageTitle pageTitle="Staff Augmentation" pageDesc="Lorem Ipsum" />
-        <About />
-        <MoreToDiscover />
-        <ServicesOne />
-        <WhyChooseUs />
+        <PageTitle pageTitle="Staff Augmentation" pageDesc="" />
+        <StaffAugAbout />
+        <BenefitsOfHiring />
+        <ExploreOurTask />
+        <WhyChooseAntixx />
         <BestSupport />
         <FreeTrial />
         <FooterStyleOne />
@@ -14,50 +14,22 @@
 
 <script>
 import NavbarStyleTwo from '../layouts/NavbarStyleTwo';
-import PageTitle from '../components/staff-augmentation/PageTitle';
-import AmazingFeatures from '../components/staff-augmentation/AmazingFeatures';
-import ServicesOne from '../components/staff-augmentation/ServicesOne';
-import MoreToDiscover from '../components/staff-augmentation/MoreToDiscover';
-import Services from '../components/staff-augmentation/Services';
-import YourApplication from '../components/staff-augmentation/YourApplication';
-import TheAbilityToElicit from '../components/staff-augmentation/TheAbilityToElicit';
-import TheBestInnovative from '../components/staff-augmentation/TheBestInnovative';
-import WhyChooseUs from '../components/staff-augmentation/WhyChooseUs';
-import Features from '../components/staff-augmentation/Features';
-import ConnectionsYourCustomers from '../components/staff-augmentation/ConnectionsYourCustomers';
-import About from '../components/staff-augmentation/About';
-import PageAbout from '../components/staff-augmentation/PageAbout';
-import CaseStudies from '../components/staff-augmentation/CaseStudies';
-import Featured from '../components/staff-augmentation/Featured';
-import PageFeature from '../components/staff-augmentation/PageFeature';
-import Expertise from '../components/staff-augmentation/Expertise';
-import Faq from '../components/staff-augmentation/Faq';
-import Pricing from '../components/staff-augmentation/Pricing';
-import BestSupport from '../components/staff-augmentation/BestSupport';
-import FreeTrial from '../components/staff-augmentation/FreeTrial';
+import PageTitle from '../components/Common/PageTitle';
+import StaffAugAbout from '../components/staff-augmentation/StaffAugAbout';
+import BenefitsOfHiring from '../components/staff-augmentation/BenefitsOfHiring';
+import ExploreOurTask from '../components/staff-augmentation/ExploreOurTask';
+import WhyChooseAntixx from '../components/staff-augmentation/WhyChooseAntixx';
+import BestSupport from '../components/Common/BestSupport';
+import FreeTrial from '../components/Common/FreeTrial';
 import FooterStyleOne from '../layouts/FooterStyleOne';
 export default {
     components: {
         NavbarStyleTwo,
         PageTitle,
-        AmazingFeatures,
-        ServicesOne,
-        Services,
-        MoreToDiscover,
-        YourApplication,
-        TheAbilityToElicit,
-        WhyChooseUs,
-        Features,
-        ConnectionsYourCustomers,
-        TheBestInnovative,
-        About,
-        PageAbout,
-        PageFeature,
-        CaseStudies,
-        Featured,
-        Expertise,
-        Faq,
-        Pricing,
+        StaffAugAbout,
+        BenefitsOfHiring,
+        ExploreOurTask,
+        WhyChooseAntixx,
         BestSupport,
         FreeTrial,
         FooterStyleOne,
