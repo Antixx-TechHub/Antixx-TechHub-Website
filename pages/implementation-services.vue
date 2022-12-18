d60543670147bb5d2a305edcb2986974830da266
AntixxTechHub <info@antixxtechhub.com>
2022-12-18 17:07:15 +0530

    Implementation Services page filtered 18.12.22

--- pages/implementation-services.vue
+++ pages/implementation-services.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <NavbarStyleTwo />
-        <PageTitle pageTitle="Implementation Services" pageDesc="Lorem Ipsum" />
-        <About />
-        <PageAbout />
-        <ServicesOne />
-        <WhyChooseUs />
-        <TheAbilityToElicit />
-        <PageFeature />
-        <Expertise />
+        <PageTitle pageTitle="Implementation Services" pageDesc="" />
+        <ImplementationAbout />
+        <WhyYouNeed />
+        <WhatServices />
+        <ExpertServices />
+        <ToolsAndTechnologies />
+        <OurApproach />
+        <WhyHireAntixxTechhub />
         <BestSupport />
         <FreeTrial />
         <FooterStyleOne />
@@ -17,50 +17,28 @@
 
 <script>
 import NavbarStyleTwo from '../layouts/NavbarStyleTwo';
-import PageTitle from '../components/implementation-services/PageTitle';
-import AmazingFeatures from '../components/implementation-services/AmazingFeatures';
-import ServicesOne from '../components/implementation-services/ServicesOne';
-import MoreToDiscover from '../components/implementation-services/MoreToDiscover';
-import Services from '../components/implementation-services/Services';
-import YourApplication from '../components/implementation-services/YourApplication';
-import TheAbilityToElicit from '../components/implementation-services/TheAbilityToElicit';
-import TheBestInnovative from '../components/implementation-services/TheBestInnovative';
-import WhyChooseUs from '../components/implementation-services/WhyChooseUs';
-import Features from '../components/implementation-services/Features';
-import ConnectionsYourCustomers from '../components/implementation-services/ConnectionsYourCustomers';
-import About from '../components/implementation-services/About';
-import PageAbout from '../components/implementation-services/PageAbout';
-import CaseStudies from '../components/implementation-services/CaseStudies';
-import Featured from '../components/implementation-services/Featured';
-import PageFeature from '../components/implementation-services/PageFeature';
-import Expertise from '../components/implementation-services/Expertise';
-import Faq from '../components/implementation-services/Faq';
-import Pricing from '../components/implementation-services/Pricing';
-import BestSupport from '../components/implementation-services/BestSupport';
-import FreeTrial from '../components/implementation-services/FreeTrial';
+import PageTitle from '../components/Common/PageTitle';
+import ImplementationAbout from '../components/implementation-services/ImplementationAbout';
+import WhyYouNeed from '../components/implementation-services/WhyYouNeed';
+import WhatServices from '../components/implementation-services/WhatServices';
+import ExpertServices from '../components/implementation-services/ExpertServices';
+import ToolsAndTechnologies from '../components/implementation-services/ToolsAndTechnologies';
+import OurApproach from '../components/implementation-services/OurApproach';
+import WhyHireAntixxTechhub from '../components/implementation-services/WhyHireAntixxTechhub';
+import BestSupport from '../components/Common/BestSupport';
+import FreeTrial from '../components/Common/FreeTrial';
 import FooterStyleOne from '../layouts/FooterStyleOne';
 export default {
     components: {
         NavbarStyleTwo,
         PageTitle,
-        AmazingFeatures,
-        ServicesOne,
-        Services,
-        MoreToDiscover,
-        YourApplication,
-        TheAbilityToElicit,
-        WhyChooseUs,
-        Features,
-        ConnectionsYourCustomers,
-        TheBestInnovative,
-        About,
-        PageAbout,
-        PageFeature,
-        CaseStudies,
-        Featured,
-        Expertise,
-        Faq,
-        Pricing,
+        ImplementationAbout,
+        WhyYouNeed,
+        WhatServices,
+        ExpertServices,
+        ToolsAndTechnologies,
+        OurApproach,
+        WhyHireAntixxTechhub,
         BestSupport,
         FreeTrial,
         FooterStyleOne,
