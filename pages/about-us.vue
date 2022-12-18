eeada553c33f27ea934cd174a6e813fd9b721009
AntixxTechHub <info@antixxtechhub.com>
2022-12-18 17:34:11 +0530

    About Us page filtered 18.12.22

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,14 +1,13 @@
 <template>
     <div>
         <NavbarStyleTwo />
-        <PageTitle pageTitle="About Us" pageDesc="Lorem Ipsum" />
-        <Expertise />
-        <WhyChooseUs />
-        <Features />
-        <About />
-        <PageAbout />
-        <Despite />
-        <Faq />
+        <PageTitle pageTitle="About Us" pageDesc="" />
+        <AboutUs />
+        <EthicalValues />
+        <VisionMission />
+        <OrganizationalCulture />
+        <OurServices />
+        <DespiteMaintaining />
         <BestSupport />
         <FreeTrial />
         <FooterStyleOne />
@@ -17,29 +16,27 @@
 
 <script>
 import NavbarStyleTwo from '../layouts/NavbarStyleTwo';
-import PageTitle from '../components/about-us/PageTitle'
-import Expertise from '../components/about-us/Expertise';;
-import WhyChooseUs from '../components/about-us/WhyChooseUs';
-import Features from '../components/about-us/Features';
-import About from '../components/about-us/About';
-import PageAbout from '../components/about-us/PageAbout';
-import Despite from '../components/about-us/Despite';
-import Faq from '../components/about-us/Faq';
-import BestSupport from '../components/about-us/BestSupport';
-import FreeTrial from '../components/about-us/FreeTrial';
+import PageTitle from '../components/Common/PageTitle'
+import AboutUs from '../components/about-us/AboutUs';
+import EthicalValues from '../components/about-us/EthicalValues';
+import VisionMission from '../components/about-us/VisionMission';
+import OrganizationalCulture from '../components/about-us/OrganizationalCulture';
+import OurServices from '../components/about-us/OurServices';
+import DespiteMaintaining from '../components/about-us/DespiteMaintaining';
+import BestSupport from '../components/Common/BestSupport';
+import FreeTrial from '../components/Common/FreeTrial';
 import FooterStyleOne from '../layouts/FooterStyleOne';
 
 export default {
     components: {
         NavbarStyleTwo,
         PageTitle,
-        Expertise,
-        WhyChooseUs,
-        Features,
-        About,
-        PageAbout,
-        Despite,
-        Faq,
+        AboutUs,
+        EthicalValues,
+        VisionMission,
+        OrganizationalCulture,
+        OurServices,
+        DespiteMaintaining,
         BestSupport,
         FreeTrial,
         FooterStyleOne,
