cfd5b58840a300298bd70a8d8eff529ca2906fbd
AntixxTechHub <info@antixxtechhub.com>
2022-12-18 16:28:45 +0530

    Big data page filtered 18.12.22

--- pages/big-data-solutions.vue
+++ pages/big-data-solutions.vue
@@ -1,16 +1,15 @@
 <template>
     <div>
         <NavbarStyleTwo />
-        <PageTitle pageTitle="Big Data Solutions" pageDesc="Lorem Ipsum" />
-        <About />
-        <MoreToDiscover />
-        <PageFeature />
-        <ServicesOne />
-        <AmazingFeatures />
-        <WhyChooseUs />
-        <TheBestInnovative />
-        <ConnectionsYourCustomers />
-        <!-- <Faq /> -->
+        <PageTitle pageTitle="Big Data Solutions" pageDesc="" />
+        <BigDataAbout />
+        <WhatIsBigData />
+        <BigDataImplementation />
+        <WeUseTheMost />
+        <TheBenefitsOfHiring />
+        <TheKeyBenefits />
+        <WhyHireBigData />
+        <Howimportant />
         <BestSupport />
         <FreeTrial />
         <FooterStyleOne />
@@ -19,51 +18,31 @@
 
 <script>
 import NavbarStyleTwo from '../layouts/NavbarStyleTwo';
-import PageTitle from '../components/big-data-solutions/PageTitle';
-import AmazingFeatures from '../components/big-data-solutions/AmazingFeatures';
-import ServicesOne from '../components/big-data-solutions/ServicesOne';
-import MoreToDiscover from '../components/big-data-solutions/MoreToDiscover';
-import Services from '../components/big-data-solutions/Services';
-import YourApplication from '../components/big-data-solutions/YourApplication';
-import TheAbilityToElicit from '../components/big-data-solutions/TheAbilityToElicit';
-import TheBestInnovative from '../components/big-data-solutions/TheBestInnovative';
-import WhyChooseUs from '../components/big-data-solutions/WhyChooseUs';
-import Features from '../components/big-data-solutions/Features';
-import ConnectionsYourCustomers from '../components/big-data-solutions/ConnectionsYourCustomers';
-import About from '../components/big-data-solutions/About';
-import PageAbout from '../components/big-data-solutions/PageAbout';
-import CaseStudies from '../components/big-data-solutions/CaseStudies';
-import Featured from '../components/big-data-solutions/Featured';
-import PageFeature from '../components/big-data-solutions/PageFeature';
-import Expertise from '../components/big-data-solutions/Expertise';
-import Faq from '../components/big-data-solutions/Faq';
-import Pricing from '../components/big-data-solutions/Pricing';
-import BestSupport from '../components/big-data-solutions/BestSupport';
-import FreeTrial from '../components/big-data-solutions/FreeTrial';
+import PageTitle from '../components/Common/PageTitle';
+import BigDataAbout from '../components/big-data-solutions/BigDataAbout';
+import WhatIsBigData from '../components/big-data-solutions/WhatIsBigData';
+import BigDataImplementation from '../components/big-data-solutions/BigDataImplementation';
+import WeUseTheMost from '../components/big-data-solutions/WeUseTheMost';
+import TheBenefitsOfHiring from '../components/big-data-solutions/TheBenefitsOfHiring';
+import TheKeyBenefits from '../components/big-data-solutions/TheKeyBenefits';
+import WhyHireBigData from '../components/big-data-solutions/WhyHireBigData';
+import Howimportant from '../components/big-data-solutions/Howimportant';
+import BestSupport from '../components/Common/BestSupport';
+import FreeTrial from '../components/Common/FreeTrial';
 import FooterStyleOne from '../layouts/FooterStyleOne';
 
 export default {
     components: {
         NavbarStyleTwo,
         PageTitle,
-        AmazingFeatures,
-        ServicesOne,
-        Services,
-        MoreToDiscover,
-        YourApplication,
-        TheAbilityToElicit,
-        WhyChooseUs,
-        Features,
-        ConnectionsYourCustomers,
-        TheBestInnovative,
-        About,
-        PageAbout,
-        PageFeature,
-        CaseStudies,
-        Featured,
-        Expertise,
-        Faq,
-        Pricing,
+        BigDataAbout,
+        WhatIsBigData,
+        BigDataImplementation,
+        WeUseTheMost,
+        TheBenefitsOfHiring,
+        TheKeyBenefits,
+        WhyHireBigData,
+        Howimportant,
         BestSupport,
         FreeTrial,
         FooterStyleOne,
