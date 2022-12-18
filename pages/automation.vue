686629cfbf8d72bbe6a31610867fe1252e5f263e
AntixxTechHub <info@antixxtechhub.com>
2022-12-18 16:03:22 +0530

    Automation page filtered 18.12.22

--- pages/automation.vue
+++ pages/automation.vue
@@ -1,14 +1,13 @@
 <template>
     <div>
         <NavbarStyleTwo />
-        <PageTitle pageTitle="Automation" pageDesc="Lorem Ipsum" />
-        <About />
-        <WhyChooseUs />
-        <MoreToDiscover />
-        <ServicesOne />
-        <AmazingFeatures />
-        <Expertise />
-        <!-- <Faq /> -->
+        <PageTitle pageTitle="Automation" pageDesc="" />
+        <AutomationAbout />
+        <BenefitsofHiring />
+        <WhatWeDoAsBA />
+        <OurTechnicalExcellence />
+        <IndustriesWeServe />
+        <WhyChooseAntixx />
         <BestSupport />
         <FreeTrial />
         <FooterStyleOne />
@@ -17,51 +16,27 @@
 
 <script>
 import NavbarStyleTwo from '../layouts/NavbarStyleTwo';
-import PageTitle from '../components/automation/PageTitle';
-import AmazingFeatures from '../components/automation/AmazingFeatures';
-import ServicesOne from '../components/automation/ServicesOne';
-import MoreToDiscover from '../components/automation/MoreToDiscover';
-import Services from '../components/automation/Services';
-import YourApplication from '../components/automation/YourApplication';
-import TheAbilityToElicit from '../components/automation/TheAbilityToElicit';
-import TheBestInnovative from '../components/automation/TheBestInnovative';
-import WhyChooseUs from '../components/automation/WhyChooseUs';
-import Features from '../components/automation/Features';
-import ConnectionsYourCustomers from '../components/automation/ConnectionsYourCustomers';
-import About from '../components/automation/About';
-import PageAbout from '../components/automation/PageAbout';
-import CaseStudies from '../components/automation/CaseStudies';
-import Featured from '../components/automation/Featured';
-import PageFeature from '../components/automation/PageFeature';
-import Expertise from '../components/automation/Expertise';
-import Faq from '../components/automation/Faq';
-import Pricing from '../components/automation/Pricing';
-import BestSupport from '../components/automation/BestSupport';
-import FreeTrial from '../components/automation/FreeTrial';
+import PageTitle from '../components/Common/PageTitle';
+import AutomationAbout from '../components/automation/AutomationAbout';
+import BenefitsofHiring from '../components/automation/BenefitsofHiring';
+import WhatWeDoAsBA from '../components/automation/WhatWeDoAsBA';
+import OurTechnicalExcellence from '../components/automation/OurTechnicalExcellence';
+import IndustriesWeServe from '../components/automation/IndustriesWeServe';
+import WhyChooseAntixx from '../components/automation/WhyChooseAntixx';
+import BestSupport from '../components/Common/BestSupport';
+import FreeTrial from '../components/Common/FreeTrial';
 import FooterStyleOne from '../layouts/FooterStyleOne';
 
 export default {
     components: {
         NavbarStyleTwo,
         PageTitle,
-        AmazingFeatures,
-        ServicesOne,
-        Services,
-        MoreToDiscover,
-        YourApplication,
-        TheAbilityToElicit,
-        WhyChooseUs,
-        Features,
-        ConnectionsYourCustomers,
-        TheBestInnovative,
-        About,
-        PageAbout,
-        PageFeature,
-        CaseStudies,
-        Featured,
-        Expertise,
-        Faq,
-        Pricing,
+        AutomationAbout,
+        BenefitsofHiring,
+        WhatWeDoAsBA,
+        OurTechnicalExcellence,
+        IndustriesWeServe,
+        WhyChooseAntixx,
         BestSupport,
         FreeTrial,
         FooterStyleOne,
