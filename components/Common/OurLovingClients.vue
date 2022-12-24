226a6ecc1dd4ae6bbf4a99698d5341d28f3c77bf
AntixxTechHub <info@antixxtechhub.com>
2022-12-24 22:59:25 +0530

    Loving client api update

--- components/Common/OurLovingClients.vue
+++ components/Common/OurLovingClients.vue
@@ -7,15 +7,14 @@
 
             <div 
                 class="clients-logo-list align-items-center"
-                v-if="clients !== []"
+                v-if="partners !== null"
             >
                 <div 
                     class="single-clients-logo"
-                    v-for="client in clients"
-                    :key="client.id"
+                    v-for="slide in partners.partnerSlides" :key="slide.id"
                 >
                     <a href="#">
-                        <img :src="client.img.url" alt="image">
+                        <img :src="slide.image.data.attributes.url" alt="image">
                     </a>
                 </div>
             </div>
@@ -24,15 +23,22 @@
 </template>
 
 <script>
+
+import axios from 'axios'
+
 export default {
     name: 'OurLovingClients',
-    data(){
-        return{
-            clients: []
-        }
+    data: () => ({
+        settings: {
+            itemsToShow: 1,
+            snapAlign: 'center',
+        },
+        partners: null,
+    }),
+    created: async function () {
+        const response = await axios.get('http://localhost:1337/api/partner?populate=partnerSlides.image')
+        const { data: { attributes } } = response.data
+        this.partners = attributes
     },
-    created: async function(){
-        this.clients = await this.$strapi.find('lovingclientscards')
-    }
 }
 </script>
